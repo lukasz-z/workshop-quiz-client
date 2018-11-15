ec96c24039269a1a006f20fb74cc513fb5051065
Łukasz Zarzecki <Lukasz_Zarzecki@mckinsey.com>
2018-11-15 12:43:40 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './styles/normalize.css'
+import './styles/skeleton.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main-app container">
+        <h1>React Workshop</h1>
       </div>
     );
   }
